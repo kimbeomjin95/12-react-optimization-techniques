08794f9023f783532a067afefa96b26092c6abad
kimbeomjin95 <kimbeomjin95@github.com>
2022-01-03 21:56:00 +0900

    162. useCallback()으로 함수 재생성 방지하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.css';
 import Button from './components/UI/Button/Button';
@@ -9,9 +9,9 @@ function App() {
 
   console.log('app running')
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph(prevshowParagraph => !prevshowParagraph);
-  }
+  }, []);
 
   return (
     <div className="app">
