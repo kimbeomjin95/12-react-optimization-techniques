e9036ead2e7baea7fea2760ac67b3c2363fe8c16
kimbeomjin95 <kimbeomjin95@github.com>
2022-01-03 22:28:25 +0900

    163. useCallback() 및 해당 종속성

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import Button from './components/UI/Button/Button';
 import DemoOuput from './Demo/DemoOuput';
 
 function App() {
-  const [showParagraph,setShowParagraph] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  console.log('app running')
+  console.log('app running');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph(prevshowParagraph => !prevshowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph(prevshowParagraph => !prevshowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOuput show={false}/>
+      <DemoOuput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
